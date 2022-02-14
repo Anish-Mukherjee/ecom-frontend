689f13c79551f682eae3f862fdf90d30680cb15b
Anish Mukherjee <cybertronian217@gmail.com>
2022-02-14 13:04:56 +0530

    refactor

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Cta from "./components/Cta";
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import Home from "./components/Home";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <Cta />
+    <div>
+      <Navbar />  
     </div>
   );
 }
